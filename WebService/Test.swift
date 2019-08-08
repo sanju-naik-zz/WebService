f60d7d9546ddb0c5879158f469730ff48341ad30
Sanju Naik <sanju.naik@go-jek.com>
2019-08-08 20:27:46 +0530

    add test struct

---
+++ WebService/Test.swift
@@ -0,0 +1,19 @@
+//
+//  Test.swift
+//  WebService
+//
+//  Created by Sanju Naik on 08/08/19.
+//  Copyright © 2019 Sanju. All rights reserved.
+//
+
+import Foundation
+
+public struct TestAddingNew {
+    public let a: String
+    public let b: String
+    
+    public init(a: String, b: String) {
+        self.a = a
+        self.b = b
+    }
+}
